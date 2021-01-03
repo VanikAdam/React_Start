fc6126113dc995add03e2f08bfa6d0ce4bc6ee50
VanikAdam <vanik.adamyan.95@gmail.com>
2021-01-03 21:56:01 +0400

    Styleing

--- src/components/todo/ToDo.jsx
+++ src/components/todo/ToDo.jsx
@@ -33,53 +33,47 @@ class ToDo extends Component {
         })
     }
 
-    // getDate = () => {
-    //     let api = {
-    //         key: '8efae59934ba47a35fd0964006fe0438',
-    //         baseUrl: 'https://api.openweathermap.org/data/2.5/forecast?'
-    //     };
-    //     const latitude = 40.17;
-    //     const longitude = 44.49;
-    //     const url = `${api.baseUrl}lat=${latitude}&lon=${longitude}&exclude=daily&appid=${api.key}`;
-
-    //     fetch(url).then(data => data.json()).then(data =>this.setState({ list: data.list }) );
-
-
-    // }
+    
     addTask = (currentValue, date) => {
 
         let currentTasks = [...this.state.tasks];
         let addedTask = [currentValue, date];
-        // currentTasks.push(addedTask);
-        // this.getDate();
-
-        // this.setState({
-        //     tasks: currentTasks,
-        //     currentValue: ''
-        // })
+       
 
         let list = [...this.state.list];
 
 
-        console.log(this.state.list)
-
+    
+        let find=false;
+        let icon;
+        let weather='';
         for (let i = 0; i < list.length; i++) {
             let dt = list[i].dt_txt.slice(0, 10)
-            // console.log(list[i].dt_txt.slice(0,10));
+            
+            
             if (date === dt) {
-                let icon = list[i].weather[0].icon;
+                find=true;
+                 icon = list[i].weather[0].icon;
+                 find=true;
                 if (icon.slice(-1) === 'd') {
                     icon = 'P'+list[i+1].weather[0].icon;
-                    let weather=Math.floor(list[i+1].main.temp-273);
+                     weather=Math.floor(list[i+1].main.temp-273);
                     weather+='°C';
-                    addedTask.push(icon,weather);
+                   
                   
                     break;
                 }
               
             }
+            
+            
         }
+if(!find){
+                icon='Punknown';
+                weather='Unknown'
+            }
 
+            addedTask.push(icon,weather);
         currentTasks.push(addedTask);
         this.setState({
             tasks: currentTasks,
@@ -96,13 +90,22 @@ class ToDo extends Component {
       
         let li = this.state.tasks.map((elem, index) => {
 
-            return <li key={index} className={style.list} >{elem[0]}<br/>{elem[1]} <span id={index} onClick={() => this.removeTask(index)}>X</span>
+            return <li key={index} className={style.list} >
+                <div className={style.listContent}>
+                     <p className={style.taskText}>{elem[0]}</p>
+                <p>{elem[1]} </p>
+                <span id={index} onClick={() => this.removeTask(index)}>Skip</span>
          
 
      
      
-            <p className={style.temp}>{elem[3]}</p>
-            <img src={images[elem[2]]} alt=""/>
+            
+                </div>
+               <div className={style.weatherSide}>
+               <img src={images[elem[2]]} alt="" className={style.weatherIcon}/>
+               <p className={style.temp}>{elem[3]}</p>
+               </div>
+            
             </li>
         })
        
